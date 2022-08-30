594f3dc5034859eafbce5fde5dc76a30a3adfd7b
Matthieu Daulhiac <matthieu@blanknetwork.com>
2022-08-30 14:39:42 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vanilla Typescript App
+
+## Installation
+
+```
+cd traffic-run-game
+yarn install
+```
+
+## Getting Started
+
+```
+yarn start
+```
+
+## Notes
+
+This project is a base repository for vanilla Typescript projects
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import decrement from "./helpers/decrement";
+import increment from "./helpers/increment";
+
+// Get the different element from the document
+const countValue = <HTMLSpanElement>document.querySelector("#count-value");
+const incrementBtn = <HTMLButtonElement>(
+  document.querySelector("#increment-btn")
+);
+const decrementBtn = <HTMLButtonElement>(
+  document.querySelector("#decrement-btn")
+);
+
+// Handle the click on increment Button
+function handleIncrementClick() {
+  const currentValue = parseInt(countValue.innerText);
+  countValue.innerText = increment(currentValue).toString();
+}
+
+// Handle the click on decrement Button
+function handleDecrementClick() {
+  const currentValue = parseInt(countValue.innerText);
+  countValue.innerText = decrement(currentValue).toString();
+}
+
+// Add event listener to the buttons
+incrementBtn.addEventListener("click", () => handleIncrementClick());
+decrementBtn.addEventListener("click", () => handleDecrementClick());
