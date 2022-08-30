c71152262ba7ee698688fe2dd8af86ccae0a0f12
Matthieu Daulhiac <matthieu@blanknetwork.com>
2022-08-30 16:09:52 +0800

    fic pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-	`./node_modules/.bin/eslint --fix ${filenames.map((f) =>
-		path.relative(process.cwd(), f)
-	)}`;
+	`./node_modules/.bin/eslint --fix ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' ')}`;
 
 module.exports = {
-	'!(public/static/bundle/**/*)*.{js,jsx,ts,tsx}':
-		'./node_modules/.bin/eslint --fix',
+	'!(public/static/bundle/**/*)*.{js,jsx,ts,tsx}': [buildEslintCommand],
 	'!(public/static/bundle/**/*)*.{md,json,html,js,jsx,ts,tsx}':
 		'./node_modules/.bin/prettier --write',
 };
